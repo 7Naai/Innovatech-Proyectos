4e7f4cf3575474313f5ef858ac81b36c0d7118ff
7Naai <iangomezchacon@gmail.com>
2026-04-21 17:53:17 -0400

    Creacion de Models
    Models de Asignacion, Proyecto y Tarea

--- src/main/java/com/example/proyectos/model/Asignacion.java
+++ src/main/java/com/example/proyectos/model/Asignacion.java
@@ -1,5 +1,30 @@
 package com.example.proyectos.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "asignaciones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Asignacion {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_asignacion")
+    private Long idAsignacion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tarea", nullable = false)
+    private Tarea tarea;
+
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "fecha_asignacion")
+    private LocalDate fechaAsignacion;
 }
