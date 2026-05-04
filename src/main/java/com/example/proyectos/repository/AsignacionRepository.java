68c4d4319c9ccc9f8300a990387d00340709f823
7Naai <iangomezchacon@gmail.com>
2026-05-04 12:49:55 -0400

    Update Repository Asignacion

--- src/main/java/com/example/proyectos/repository/AsignacionRepository.java
+++ src/main/java/com/example/proyectos/repository/AsignacionRepository.java
@@ -1,5 +1,16 @@
 package com.example.proyectos.repository;
 
-public class AsignacionRepository {
-    
+import com.example.proyectos.model.Asignacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AsignacionRepository extends JpaRepository<Asignacion, Long> {
+
+    List<Asignacion> findByIdUsuario(Long idUsuario);
+
+    List<Asignacion> findByTarea_IdTarea(Long idTarea);
+
+    // Por si necesitamos buscar por horas
+    List<Asignacion> findByHorasAproximadas(Integer horasAproximadas);
 }
