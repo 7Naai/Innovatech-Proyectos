92fc12590e0efa13bed7e32ba18b58a2828228a5
7Naai <iangomezchacon@gmail.com>
2026-06-01 15:18:29 -0400

    Update rata

--- src/main/java/com/example/proyectos/ProyectosApplication.java
+++ src/main/java/com/example/proyectos/ProyectosApplication.java
@@ -1,13 +1,18 @@
 package com.example.proyectos;
 
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProyectosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectosApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProyectosApplication.class, args);
+  }
+  @Bean
+  public ApplicationRunner initRabbit(RabbitAdmin rabbitAdmin) {
+    return args -> rabbitAdmin.initialize();
+  }
 }
